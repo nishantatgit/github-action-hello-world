5be677fc172c7a4d0be898b37cd3b97d6945ebd4
nishantatgit <nishant.kumar.shreevastav@gmail.com>
2022-04-04 17:12:04 +0530

    modified action

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+	console.log('core ', core);
 	const nameToGreet= core.getInput('who-to-greet');
+	const inputList = core.getInput('inputList');
+	console.log('inputList ', inputList);
 	console.log(`Hello ${nameToGreet}!`);
 	const time = (new Date()).toTimeString();
 	core.setOutput('time',time);
